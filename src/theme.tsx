6a278352b255da9b4e15dfcb20c59af726384852
Byevenes <zneel.yevenes@gmail.com>
2022-08-18 20:23:03 -0400

    add dark theme for default

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,6 +1,12 @@
 import { extendTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+}
+
 const fonts = { mono: `'Menlo', monospace` }
 
 const breakpoints = createBreakpoints({
@@ -19,6 +25,7 @@ const theme = extendTheme({
   },
   fonts,
   breakpoints,
+  config,
 })
 
 export default theme
