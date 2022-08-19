68d306cbcc3570341dd02f03f9e89a1b9e9accf4
Byevenes <zneel.yevenes@gmail.com>
2022-08-18 20:28:09 -0400

    add _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Navbar } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
