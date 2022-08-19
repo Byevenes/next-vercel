00d104534685e7bf88ea71c67c12ef1234618391
Byevenes <zneel.yevenes@gmail.com>
2022-08-18 20:20:12 -0400

    change folders

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { MainLayout } from '../components/layouts/MainLayout'
+import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider resetCSS theme={theme}>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
